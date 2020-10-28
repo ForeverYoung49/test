520faac346820b4e2d0cb5930e7a7af9939e82c9
ForeverYoung <ForeverYoung49@users.noreply.github.com>
2020-10-28 22:28:17 +0500

    main

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import FadeIn from 'react-fade-in';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {letters: []};
+      this.handleEvent = this.handleEvent.bind(this);
+  }
+  handleEvent(e) {
+      if (e.key.toUpperCase() !== e.key.toLowerCase() && e.key.length === 1) {
+          let newLetters = this.state.letters.slice();
+          newLetters.push(e.key);
+          this.setState({letters: newLetters});
+      }
+  }
+  render() {
+      return (
+          <div class="root" onKeyDown={this.handleEvent} tabindex="0">
+          {
+              this.state.letters.map(letter => <FadeIn transitionDuration="3000" ><div class="letter">{letter}</div></FadeIn>)
+          }
+          </div>
+      );
+  }
+}
+ReactDOM.render(<App/>, document.getElementById("app"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
